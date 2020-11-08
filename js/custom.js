e330a27d37023f17e28fc3483b6141caf4a03592
Youngislander <yjmnopainnogain@gmail.com>
2020-11-08 16:34:23 +0900

    map

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
+
 const logo = document.querySelector(".main_txt");
-window.addEventListener("load", function(){
     logo.setAttribute("class","main_txt on")
-});
+
 
 $(function(){
     $('.qna_list li').click(function(){
@@ -296,3 +296,11 @@ var scene_q_line = new ScrollMagic.Scene({
 })
 
 AOS.init();
+
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+// tl.to(".intro", { y: "-100%", duration: 1}, "-=1");
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
